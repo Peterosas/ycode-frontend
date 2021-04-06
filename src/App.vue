2d1591cbf25db235f247567a9576f5eece96fa23
Vincent <TRC@Vincents-MacBook-Pro.local>
2021-04-06 10:59:03 -0500

    Add test template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="h-screen w-screen overflow-hidden bg-white flex flex-col">
+    <NavBar />
+
+    <section class="relative flex flex-1 flex-row overflow-hidden">
+    <Layers />
+      <section
+        class="w-full h-full relative z-10 scrollbar text-center text-none overflow-auto"
+      >
+        <button @click="createLayer">add layer</button>
+
+        <div ref="layers"></div>
+      </section>
+
+      <Design />
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Design from "./components/Design.vue";
+import Layers from "./components/Layers.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Design,
+    NavBar,
+    Layers
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  methods: {
+    createLayer() {
+      this.$refs.layers.innerHTML = `<div>Hi, I am a layer 👋🏼</div>`;
+    },
+  },
+};
+</script>
